10b9fc1363b5ea49939bc5da5033c505f6218dae
Krityan Kashyap <krityankashyap@Krityans-MacBook-Air.local>
2025-05-28 19:32:06 +0530

    Added redis config

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,7 +2,8 @@
 import dotenv from 'dotenv';
 
 type ServerConfig = {
-    PORT: number
+    PORT: number,
+    REDIS_SERVER_URL: string
 }
 
 function loadEnv() {
@@ -13,5 +14,6 @@ function loadEnv() {
 loadEnv();
 
 export const serverConfig: ServerConfig = {
-    PORT: Number(process.env.PORT) || 3001
+    PORT: Number(process.env.PORT) || 3001,
+    REDIS_SERVER_URL: process.env.REDIS_SERVER_URL || 'redis://localhost:6379'
 };
