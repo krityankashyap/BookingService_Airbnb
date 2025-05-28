def0217abf1cdf4720768925a60752aa4844e70f
Krityan Kashyap <krityankashyap@Krityans-MacBook-Air.local>
2025-05-28 21:19:24 +0530

    Added redis lock and ttl(time to live)

--- src/services/booking.services.ts
+++ src/services/booking.services.ts
@@ -1,11 +1,26 @@
 import { CreateBookingDTO } from "../dto/booking.dto";
 import { confirmBooking, createBooking, createIdempotencyKey, FinalizedIdempotencyKey,  getIdempotencyKeywithLock } from "../repositories/booking.repository";
-import { BadRequestError, NotFoundError } from "../utils/errors/app.error";
+import { BadRequestError, InternalServerError, NotFoundError } from "../utils/errors/app.error";
 import { generateIdempotencyKey } from "../utils/generateIdempotency";
 
 import  prismaClient  from "../prisma/client";
+import { redlock } from "../config/redis.config";
 
 export async function createBookingService(createBookingDTO: CreateBookingDTO){
+
+ // In the booking service we need to ensure is that when we try to create a booking before we should try to lock the booking
+
+  const ttl = 5000 * 60; // airbnb is booked for 5 min in pending state and nobody can book that airbnb if we don't book bnb before 5 min then it will be gone to cancelled state
+  
+  const bookingResource = `hotel:${createBookingDTO.hotelId}`; // this is the string on which we gonna take a lock and it gonna define that which hotelId is locked by some user 
+
+  console.log(`Acquriing lock for resource: ${bookingResource} with ttl: ${ttl}`);
+
+  let lock;
+
+  try {
+    lock =  await redlock.acquire([bookingResource] , ttl);
+    console.log(`Lock acquired for resource: ${bookingResource}` , lock);
     const booking = await createBooking({
       userId : createBookingDTO.userId,
       hotelId : createBookingDTO.hotelId,
@@ -21,6 +36,32 @@ export async function createBookingService(createBookingDTO: CreateBookingDTO){
       idempotencyKey: idempotencyKey,
       bookingId: booking.id
     };
+
+  } catch (error) {
+    throw new InternalServerError('failed to acquire lock for booking resource');
+  }
+  
+
+  // return await redlock.using([bookingResource] , ttl , async ()=>{
+     
+   
+  //   const booking = await createBooking({
+  //     userId : createBookingDTO.userId,
+  //     hotelId : createBookingDTO.hotelId,
+  //     totalGuest : createBookingDTO.totalGuest,
+  //     bookingAmount : createBookingDTO.bookingAmount
+  //   });
+
+  //   const idempotencyKey = generateIdempotencyKey();
+
+  //   await createIdempotencyKey(idempotencyKey , booking.id);
+
+  //   return {
+  //     idempotencyKey: idempotencyKey,
+  //     bookingId: booking.id
+  //   };
+
+  // });
 };
 
 export async function confirmBookingService(idempotencyKey: string){
