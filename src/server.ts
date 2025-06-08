68f5f721fa076f159be73aea46164f869c31ca85
Krityan Kashyap <krityankashyap@Krityans-MacBook-Air.local>
2025-06-08 13:18:28 +0530

    Added asynchronous communication b/w booking and notification service

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import v2Router from './routers/v2/index.router';
 import { appErrorHandler, genericErrorHandler } from './middlewares/error.middleware';
 import logger from './config/logger.config';
 import { attachCorrelationIdMiddleware } from './middlewares/correlation.middleware';
+import { addEmailToQueue } from './producer/email.producer';
 const app = express();
 
 app.use(express.json());
@@ -29,4 +30,14 @@ app.use(genericErrorHandler);
 app.listen(serverConfig.PORT, () => {
     logger.info(`Server is running on http://localhost:${serverConfig.PORT}`);
     logger.info(`Press Ctrl+C to stop the server.`);
+
+    addEmailToQueue({
+        to: "Sample foe",
+        subject: "Asynchronous way to communicate b/w services",
+        templateId: "Sample ID",
+        params: {
+            name: "Krityan",
+            orderId: "2004",
+        },
+    });
 });
