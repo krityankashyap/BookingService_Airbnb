44a5df442a1cab8550ac8542765d2011f0c74901
Krityan Kashyap <krityankashyap@Krityans-MacBook-Air.local>
2025-06-08 13:06:55 +0530

    Added producer consumer notification service

--- src/config/redis.config.ts
+++ src/config/redis.config.ts
@@ -1,10 +1,36 @@
-import IORedis from 'ioredis'
+import IORedis, { Redis } from 'ioredis'
 import Redlock from 'redlock'
 import  { serverConfig } from '.'
 
-export const redisClient = new IORedis(serverConfig.REDIS_SERVER_URL); 
+// export const redisClient = new IORedis(serverConfig.REDIS_SERVER_URL); 
 
- export const redlock = new Redlock([redisClient], {
+// Singleton pattern to connect with Redis
+function connectionToRedis(){
+
+  try {
+ 
+   let connection: Redis;
+   
+   return () => {
+     if(!connection){
+       connection = new IORedis(serverConfig.REDIS_SERVER_URL); 
+       return connection;
+     }
+ 
+     return connection;
+   }
+ 
+  } catch (error) {
+    console.log("Error connection to reddis: ",error);
+    throw error;
+  }
+ }
+ 
+ // Singleton Object :- it is an object that is actually created once in the lifetime of ur code running and whenever someone agains tries to get that object the same already created object will returned, u don't create a brand new object for the same singleton alltogether so here we gonna make this function as singleton so that we don't need to make connection object again and again
+ 
+ export const getRedisConnectionObject = connectionToRedis();
+
+ export const redlock = new Redlock([getRedisConnectionObject()], {
 
   // The expected clock drift;
   driftFactor: 0.01, // time in ms
